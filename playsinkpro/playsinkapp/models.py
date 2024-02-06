d68c8230750e4952d9b739a782884e9b01f1c8c0
shubham bhosale <shubhambhosle556@gmail.com>
2024-02-06 19:31:16 +0530

    table modification

--- playsinkpro/playsinkapp/models.py
+++ playsinkpro/playsinkapp/models.py
@@ -1,23 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Genre(models.Model):
+class song_Genre(models.Model):
     name = models.CharField(max_length=100)
     song_name = models.CharField(max_length=100)
     artist_name = models.CharField(max_length=100)
 
 
-class Artist(models.Model):
+class song_Artist(models.Model):
     artist_name = models.CharField(max_length=100)
     song_name = models.CharField(max_length=100)
 
-
 class Song(models.Model):
     song_title = models.CharField(max_length=100)
-    artist_name = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    artist_name = models.ForeignKey(song_Artist, on_delete=models.CASCADE)
     album_name = models.CharField(max_length=100)
     length = models.DurationField()
-    song_genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    song_genre = models.ForeignKey(song_Genre, on_delete=models.CASCADE)
+
 
 class Playlist(models.Model):
     name = models.CharField(max_length=100)
